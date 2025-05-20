b803ee0039fb06d6b8353c888f90bba03687d776
louqlylik <mallika.najla1503@gmail.com>
2025-05-20 08:21:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("welcome to the skz hot megaverse web 👀❤️🖤")
+st.title("❤️Welcome To The SKZ Hot MEGAVERSE Web 👀🖤")
 st.write(
     "this web is made by a stay, HII Im Mallika from XF and leeknow is my bias"
 )
