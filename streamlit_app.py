234386ce470d1e9a1a1566fa7c0c8010b393f8da
louqlylik <mallika.najla1503@gmail.com>
2025-05-27 08:03:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.write("this web is made by a stay, HII Im Mallika from XF and leeknow is my b
 
 
 st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
- angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+angka = st.number_input("Tulis Sebuah Angka:", value=0, step=1)
 
  if (angka % 2) == 0:
    st.write(f"{angka} adalah Bilangan Genap")
