efeb3fd6916e591c0eaa35fc7e7b9bc9ffee01e5
louqlylik <mallika.najla1503@gmail.com>
2025-05-27 07:53:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,16 @@ import streamlit as st
 st.title("❤️Welcome To The SKZ Hot MEGAVERSE Web 👀🖤")
 st.write(
     "this web is made by a stay, HII Im Mallika from XF and leeknow is my bias"
-)
+
+
+
+
+
+st.title("Aplikasi #Lovestay")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil)
+ angka = st.number_input("Tulis sebuah Angka:",value=0, step=1)
+
+ if ( angka % 2) == 0:
+   st.write(f"(angka) adalah Bilangan Genap")
+else:
+   st.write(f"(angka) adalah Bilangan Ganjil")
